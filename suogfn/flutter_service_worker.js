40495bb0a918aad32324616b12edea9642013f3a
suogrd <info.arhakey@gmail.com>
2026-08-29 19:45:44 -0400

    Actualizar SUOG FN web

--- suogfn/flutter_service_worker.js
+++ suogfn/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3c18ecc9f72c67afe6d0e22eecb99c87
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "554dc7bf83512b48befce05c227b38ee",
+"flutter_bootstrap.js": "0b86b764c16824602d68144a2f15adc1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
